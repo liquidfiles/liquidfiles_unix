ce426dd67e68abd6d2b3c6bdec7cbed0709552d0
Sasun Hambardzumyan <xustup@gmail.com>
2015-06-18 10:19:47 +0400

    Corrected send_test

--- test/send_test.sh
+++ test/send_test.sh
@@ -16,7 +16,7 @@ fi
 rm -rf .tmp_test
 echo "Test PASSED."
 
-MESSAGE=`$EXEC send --to=xustup@example.com --server=$SERVER -k --api_key=$KEY --message_file="large_file" --subject="Hello!" $DIR/send_test.sh`
+MESSAGE=`$EXEC send --to=xustup@example.com --server=$SERVER -k --api_key=$KEY --message_file="$DIR/large_file" --subject="Hello!" $DIR/send_test.sh`
 test_status "Couldn't send message file.\n$MESSAGE"
 MESSAGE=${MESSAGE##* }
 
